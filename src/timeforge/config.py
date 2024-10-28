d34d38ba7b108a854accbcdc040a6dd20d65f470
Michael Hohenstein <michael@hohenste.in>
2024-10-28 18:01:09 +0100

    update link to pdf from PSE

--- src/timeforge/config.py
+++ src/timeforge/config.py
@@ -2,7 +2,8 @@
 
 from typing import Final
 
-MILOG_FORM_URL: Final = r"https://www.pse.kit.edu/downloads/Formulare/KIT%20Arbeitszeitdokumentation%20MiLoG.pdf"
+# MILOG_FORM_URL: Final = r"https://www.pse.kit.edu/downloads/Formulare/KIT%20Arbeitszeitdokumentation%20MiLoG.pdf"  # old link
+MILOG_FORM_URL: Final = r"https://www.pse.kit.edu/downloads/Formulare/PSE_Abrechnung_Arbeitszeitdokumentation_2020-04-01.pdf"
 FEDERAL_STATE: Final = "BW" # short notation for Baden-Württemberg
 
 # define the working time between 08:00 and 20:00
